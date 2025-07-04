8684a582ea5c52221d55197dd161223c753192e9
BAGPALLAB7 <bagmaster7@gmail.com>
2025-07-05 00:21:18 +0530

    added tailwind, daisyUI and navbar added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./gui/components/navbar"
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+
+      <button class="btn btn-primary">Button</button>
+
+    </>
+  )
+}
+
+export default App
